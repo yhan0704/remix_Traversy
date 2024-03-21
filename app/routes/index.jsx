4e04b062b0ea4fe7233ef22b9857fb1cb6e0881b
yhan0704 <hanyc1205@gmail.com>
2024-03-21 12:46:23 -0400

    delete exist root, leanred LiveReload and Outlet

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,5 +1,4 @@
 import { useLoaderData, json, Link } from "remix";
-
 // Loaders provide data to components and are only ever called on the server, so
 // you can connect to a database or run any server side code you want right next
 // to the component that renders it.
@@ -9,31 +8,31 @@ export let loader = () => {
     resources: [
       {
         name: "Remix Docs",
-        url: "https://remix.run/docs"
+        url: "https://remix.run/docs",
       },
       {
         name: "React Router Docs",
-        url: "https://reactrouter.com/docs"
+        url: "https://reactrouter.com/docs",
       },
       {
         name: "Remix Discord",
-        url: "https://discord.gg/VBePs6d"
-      }
+        url: "https://discord.gg/VBePs6d",
+      },
     ],
     demos: [
       {
         to: "demos/actions",
-        name: "Actions"
+        name: "Actions",
       },
       {
         to: "demos/about",
-        name: "Nested Routes, CSS loading/unloading"
+        name: "Nested Routes, CSS loading/unloading",
       },
       {
         to: "demos/params",
-        name: "URL Params and Error Boundaries"
-      }
-    ]
+        name: "URL Params and Error Boundaries",
+      },
+    ],
   };
 
   // https://remix.run/api/remix#json
@@ -44,13 +43,15 @@ export let loader = () => {
 export let meta = () => {
   return {
     title: "Remix Starter",
-    description: "Welcome to remix!"
+    description: "Welcome to remix!",
   };
 };
 
 // https://remix.run/guides/routing#index-routes
 export default function Index() {
   let data = useLoaderData();
+
+  console.log(data);
 
   return (
     <div className="remix__page">
@@ -72,7 +73,7 @@ export default function Index() {
       <aside>
         <h2>Demos In This App</h2>
         <ul>
-          {data.demos.map(demo => (
+          {data.demos.map((demo) => (
             <li key={demo.to} className="remix__page__resource">
               <Link to={demo.to} prefetch="intent">
                 {demo.name}
@@ -82,7 +83,7 @@ export default function Index() {
         </ul>
         <h2>Resources</h2>
         <ul>
-          {data.resources.map(resource => (
+          {data.resources.map((resource) => (
             <li key={resource.url} className="remix__page__resource">
               <a href={resource.url}>{resource.name}</a>
             </li>

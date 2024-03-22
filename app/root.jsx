abdda73e53bb4044e669474fd48687bec8663e21
yhan0704 <hanyc1205@gmail.com>
2024-03-22 09:24:36 -0400

    out date remix version, will do v2

--- app/root.jsx
+++ app/root.jsx
@@ -1,15 +1,63 @@
-import { Outlet, LiveReload } from "@remix-run/react";
+import { Outlet, LiveReload, Link, Meta, Links } from "@remix-run/react";
+import globalStylesUrl from "~/styles/global.css";
+
+export const links = () => [{ rel: "stylesheet", href: globalStylesUrl }];
+
+export const meta = () => {
+  const description = "A cool blog built with Remix";
+  const keywords = "remix, react, javascript";
+
+  return {
+    description,
+    keywords,
+  };
+};
 
 export default function App() {
   return (
+    <Document>
+      <Layout>
+        <Outlet />
+      </Layout>
+    </Document>
+  );
+}
+
+function Document({ children, title }) {
+  return (
     <html lang="en">
       <head>
-        <title>My aApp</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+        <title>{title ? title : "Remix Blog"}</title>
       </head>
       <body>
-        <Outlet />
+        {children}
         {process.env.NODE_ENV === "development" ? <LiveReload /> : null}
       </body>
     </html>
+  );
+}
+
+function Layout({ children }) {
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/" className="logo">
+          {" "}
+          Remix{" "}
+        </Link>
+
+        <ul className="nav">
+          <li>
+            <Link to="/posts">Posts</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="container">{children}</div>
+    </>
   );
 }
---
+++ app/routes/posts.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Posts() {
+  return (
+    <div>
+      <h1>This is the posts routes</h1>
+    </div>
+  );
+}
+
+export default Posts;
